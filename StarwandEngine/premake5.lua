ca69ac101bc4de92623079567425f1493e4bb611
Abhinoor Singh <abhinoor4717@gmail.com>
2026-02-05 00:15:06 -0500

    Finished basic rendering objects and OpenGL implementations

--- StarwandEngine/premake5.lua
+++ StarwandEngine/premake5.lua
@@ -19,6 +19,7 @@ project "StarwandEngine"
     includedirs {
         "vendor/glfw/include",
         "vendor/glad/include",
+        "vendor/glm",
     }
 
     defines {
--- StarwandEngine/src/StarwandEngine/Test.cpp
+++ StarwandEngine/src/StarwandEngine/Test.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 namespace StarwandEngine {
     void greet() {
@@ -28,7 +29,42 @@ namespace StarwandEngine {
             return;
         }
 
+        float vertices[] = {
+            // Vertex position      // Color
+            -0.5f, -0.5f, 1.0f,     
+            0.5f, -0.5f, 1.0f,      
+            0.5f, 0.5f, 1.0f,       
+
+            // -0.5f, -0.5f, 1.0f,     1.0f, 0.0f, 0.0f,
+            // -0.5f, 0.5f, 1.0f,      1.0f, 1.0f, 0.0f,
+            // 0.5f, 0.5f, 1.0f,       1.0f, 1.0f, 0.0f,
+        };
+
+        unsigned short indices[] = {
+                0, 1, 2
+        };
+
+        GLuint vb, ib, vl;
+
+        glGenBuffers(1, &vb);
+        glBindBuffer(GL_ARRAY_BUFFER, vb);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+        glGenBuffers(1, &ib);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ib);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+        glEnableVertexAttribArray(0);
+
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ib);
+            glBindBuffer(GL_ARRAY_BUFFER, vb);
+            glDrawRangeElements(GL_TRIANGLES, 0, 3, 3, GL_UNSIGNED_SHORT, NULL);
+
             glfwPollEvents();
             glfwSwapBuffers(window);
 
